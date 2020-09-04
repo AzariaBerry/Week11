cd38a4cf74c38fc36b404044967934b90a76bf81
Azaria berry <azariaberry2019@outlook.com>
2020-09-03 20:18:18 -0400

    still working on style

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var window = this;
 const express = require("express")
 var mysql = require('mysql');
 const app = express();
 const bodyParser  = require("body-parser");
+const nodemon = require("nodemon")
+const unirest = require('unirest')
+let responseData;
+
 
 
 // set up to read the ejs and stylesheet
@@ -20,6 +25,27 @@ var connection = mysql.createConnection({
 app.get("/", function(req, res){
     res.render("home");
 });
+
+function weatherBoy(cityID){
+    var key = '1cab01ad337737f6d48504c6a2b7d50e';
+    fetch('api.openweathermap.org/data/2.5/weather?q=' + cityID + '&appid=' + key)
+    //convert data to JSON
+    .then(function(resp) { return resp.json() })
+    .then(function(data){
+        let responseData = JSON.parse(data);
+        console.log(responseData);
+    })
+    .catch(function(){
+        alert('That city may only exsist in your mind.')
+    });
+}
+
+
+
+window.onload = function() {
+    weatherBoy(4180531);
+}
+
 
 app.listen(7878, function(){
     console.log("Server running on 7878!");
